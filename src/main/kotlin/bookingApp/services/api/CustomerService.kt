1240881db88641591d0f01bd7c87be4750e02dcb
GranPecador <lennuz17@gmail.com>
2020-11-21 14:04:13 +0300

    add customer methods

--- src/main/kotlin/bookingApp/services/api/CustomerService.kt
+++ src/main/kotlin/bookingApp/services/api/CustomerService.kt
@@ -1,10 +1,14 @@
 package bookingApp.services.api
 
+import bookingApp.repositories.entity.Dish
+
 interface CustomerService {
     fun reserveTable(tableId: Int)
     fun getRestaurants()
     fun getMenuByRestaurant(restaurantId: Int)
     fun getDishesByMenu()
+    fun doPreOrder(order: List<Dish>)
+    fun payOrder()
     fun addDishToFavorites(dishesId: Int)
     fun getFavorites()
 }

71d480fb8df296b07fde949e1cd0f45c4303bd1d
Dmitry <DiamindFaceit@yandex.ru>
2020-11-24 03:16:53 +0300

    Add filtering for restaurants

--- src/main/kotlin/bookingApp/services/RestaurantService.kt
+++ src/main/kotlin/bookingApp/services/RestaurantService.kt
@@ -2,8 +2,14 @@ package bookingApp.services
 
 import bookingApp.repositories.RestaurantRepository
 import bookingApp.repositories.entity.Restaurant
+import bookingApp.services.utils.FilterDTO
+import bookingApp.services.utils.PageRequestHolder
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
+import javax.persistence.criteria.Root
+
 
 @Service
 class RestaurantService {
@@ -19,7 +25,23 @@ class RestaurantService {
         return restaurantRepository.save(data)
     }
 
-    fun getAllRestaurants(): List<Restaurant> {
+    fun getAllRestaurants(): MutableIterable<Restaurant> {
         return restaurantRepository.findAll()
+    }
+
+    fun getByCriteria(pageRequest: PageRequestHolder): Page<Restaurant>? {
+        return restaurantRepository.findAll(processFilters(pageRequest.filters), pageRequest.pageRequest)
+    }
+
+    private fun processFilters(filters: List<FilterDTO>): Specification<Restaurant?>? {
+        var criteria = Specification.where { book: Root<Restaurant?>, _, builder -> builder.like(book.get("name"), "%%") }
+
+        for (filter in filters) {
+            when (filter.property) {
+                    "name" -> criteria = criteria.and { book, _, builder -> builder.like(book.get("name"), "%${filter.value}%") }
+            }
+        }
+
+        return criteria
     }
 }
